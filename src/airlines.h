faf4701c42a5975df7d6f00ac460efd9e33ff1cd
Dingnan Zheng <102624031+dingnan2@users.noreply.github.com>
2022-11-21 18:27:21 -0600

    Add files via upload

---
+++ src/airlines.h
@@ -0,0 +1,18 @@
+
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Airlines {
+    public:
+    Airlines();
+
+    private:
+    int airport_ID;
+    string name;
+    string city;
+    string country;
+    unsigned long
+
+};
